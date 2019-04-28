04b4ad6b52c7fd97b394572882b8fe6f343d98d7
Stefano Lupo <lupos@tcd.ie>
2019-04-28 23:38:15 +0100

    Stuff is stuffy

--- src/yolo_opencv.py
+++ src/yolo_opencv.py
@@ -1,95 +1,13 @@
-#############################################
-# Object detection - YOLO - OpenCV
-# Author : Arun Ponnusamy   (July 16, 2018)
-# Website : http://www.arunponnusamy.com
-############################################
-
-
 import cv2
 import argparse
 import numpy as np
 import glob
 import os
 
+from typing import List, Dict
 
-def get_output_layers(net):
-    
-    layer_names = net.getLayerNames()
-    
-    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+CONFIDENCE_THRESHOLD = 0.1
 
-    return output_layers
-
-def draw_prediction(classes, COLORS, img, class_id, confidence, x, y, x_plus_w, y_plus_h):
-
-    label = str(classes[class_id])
-
-    color = COLORS[class_id]
-
-    cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), color, 2)
-
-    cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-def predict_image(filename):
-
-    image = cv2.imread(filename)
-
-    Width = image.shape[1]
-    Height = image.shape[0]
-    scale = 0.00392
-
-    classes = None
-
-    with open(args.classes, 'r') as f:
-        classes = [line.strip() for line in f.readlines()]
-
-    COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
-
-    net = cv2.dnn.readNet(args.weights, args.config)
-    blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
-
-    net.setInput(blob)
-    outs = net.forward(get_output_layers(net))
-
-    class_ids = []
-    confidences = []
-    boxes = []
-    conf_threshold = 0.1
-    nms_threshold = 0.4
-
-    for out in outs:
-        for detection in out:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > 0.5:
-                center_x = int(detection[0] * Width)
-                center_y = int(detection[1] * Height)
-                w = int(detection[2] * Width)
-                h = int(detection[3] * Height)
-                x = center_x - w / 2
-                y = center_y - h / 2
-                class_ids.append(class_id)
-                confidences.append(float(confidence))
-                boxes.append([x, y, w, h])
-
-
-    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
-
-    for i in indices:
-        i = i[0]
-        box = boxes[i]
-        x = box[0]
-        y = box[1]
-        w = box[2]
-        h = box[3]
-        draw_prediction(classes, COLORS, image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
-
-    cv2.imshow("object detection", image)
-    cv2.waitKey()
-
-    # cv2.imwrite("object-detection.jpg", image)
-    # cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
@@ -108,7 +26,82 @@ if __name__ == "__main__":
     if args.dir is None and args.image is None:
         raise ValueError("Directory or image must be specified")
 
-    for fn in glob.glob(args.dir + "/*.jpg"):
-        predict_image(fn)
+    with open(args.classes, 'r') as f:
+        classes = [line.strip() for line in f.readlines()]
 
-    cv2.destroyAllWindows()
+    colours = np.random.uniform(0, 255, size=(len(classes), 3))
+
+
+def get_output_layers(net):
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    return output_layers
+
+
+def draw_prediction(classes: List[str], img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+    label = str(classes[class_id])
+    color = colours[class_id]
+    cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), color, 2)
+    cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
+def predict_image(filename, classes):
+
+    image = cv2.imread(filename)
+
+    width = image.shape[1]
+    heght = image.shape[0]
+    scale = 0.00392
+
+    net = cv2.dnn.readNet(args.weights, args.config)
+    blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+
+    net.setInput(blob)
+    outs = net.forward(get_output_layers(net))
+
+    class_ids = []
+    confidences = []
+    boxes = []
+    conf_threshold = 0.1
+    nms_threshold = 0.4
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > CONFIDENCE_THRESHOLD:
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * heght)
+                w = int(detection[2] * width)
+                h = int(detection[3] * heght)
+                x = center_x - w / 2
+                y = center_y - h / 2
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
+
+
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+    for i in indices:
+        i = i[0]
+        box = boxes[i]
+        x = box[0]
+        y = box[1]
+        w = box[2]
+        h = box[3]
+        draw_prediction(classes, image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
+
+    cv2.imshow("object detection", image)
+    cv2.waitKey()
+
+
+
+
+for fn in glob.glob(args.dir + "/*.jpg"):
+    print("running for %s" % fn)
+    predict_image(fn, classes)
+
+cv2.destroyAllWindows()
